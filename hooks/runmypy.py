f2d4d9d0029b4711dbccac7ad0c289c581a57061
Oscar Caballero <oscar.caballero@bayes.gg>
2023-09-26 17:24:50 +0200

    Move script to importable python module

---
+++ hooks/runmypy.py
@@ -0,0 +1,22 @@
+import sys
+import os
+import subprocess as sp
+import hashlib
+from pathlib import Path
+
+
+def main():
+    tmp = Path(os.environ["VIRTUAL_ENV"]).parent / "tmp"
+    tmp.mkdir(parents=True, exist_ok=True)
+    reqs = tmp / ("reqs-" + hashlib.md5(open("Pipfile.lock", "rb").read()).hexdigest())
+
+    if not reqs.is_file():
+        out = sp.check_output(["pipenv", "requirements"])
+        reqs.write_bytes(out)
+        sp.check_call(["pip", "install", "-r", str(reqs)])
+
+    sp.check_call(["mypy", *sys.argv[1:]])
+
+
+if __name__ == "__main__":
+    main()
