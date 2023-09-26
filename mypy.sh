19c00e988fe9220347f4ba3b68e697c018a4a7f0
Oscar Caballero <oscar.caballero@bayes.gg>
2023-09-26 15:49:43 +0200

    Store temp requirements in pre-commit's virtualenv

--- mypy.sh
+++ mypy.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-cd "$(dirname "$0")"
-reqs=/tmp/reqs-$(basename $PWD)-$(md5sum Pipfile.lock | awk '{print $1}')
+tmp="$(dirname $VIRTUAL_ENV)/tmp"
+mkdir -p "$tmp"
+reqs="$tmp/reqs-$(md5sum Pipfile.lock | awk '{print $1}')"
 if ! [ -f "$reqs" ]; then
-  rm -f /tmp/reqs-$(basename $PWD)-*
   pipenv requirements > $reqs
   pip install -r $reqs
 fi
-
 mypy "$@"
