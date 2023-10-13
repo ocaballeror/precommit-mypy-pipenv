aa7584792a97132e43d855caebc7b0a80a88e57a
Oscar Caballero <oscar.caballero@bayes.gg>
2023-10-13 17:35:20 +0200

    Support pyproject.toml dependencies

--- hooks/runmypy.py
+++ hooks/runmypy.py
@@ -1,3 +1,4 @@
+import tomli
 import sys
 import os
 import subprocess as sp
@@ -5,17 +6,71 @@ import hashlib
 from pathlib import Path
 
 
-def main():
+def tmpdir():
     tmp = Path(os.environ["VIRTUAL_ENV"]).parent / "tmp"
     tmp.mkdir(parents=True, exist_ok=True)
-    reqs = tmp / ("reqs-" + hashlib.md5(open("Pipfile.lock", "rb").read()).hexdigest())
+    return tmp
 
-    if not reqs.is_file():
+
+class DepHandler:
+    def supported(self) -> bool:
+        raise NotImplementedError
+
+    def requirements(self) -> str:
+        raise NotImplementedError
+
+    def reqs_hash(self) -> str:
+        raise NotImplementedError
+
+    def tmp_reqs(self) -> Path:
+        return tmpdir() / self.reqs_hash()
+
+    def updated(self) -> bool:
+        return not self.tmp_reqs().exists()
+
+    def write_reqs(self) -> None:
+        self.tmp_reqs().write_text(self.requirements())
+
+
+class Pipenv(DepHandler):
+    def supported(self) -> bool:
+        return Path("Pipfile.lock").exists()
+
+    def requirements(self) -> str:
         out = sp.check_output(
-            ["pipenv", "requirements", "--dev"], env=os.environ | {"PIPENV_VERBOSITY": "-1"}
+            ["pipenv", "requirements", "--dev"],
+            env=os.environ | {"PIPENV_VERBOSITY": "-1"},
+            text=True,
         )
-        reqs.write_bytes(out)
-        ret = sp.run(["pip", "install", "-qr", str(reqs)], text=True)
+        return out
+
+    def reqs_hash(self) -> str:
+        return hashlib.md5(open("Pipfile.lock", "rb").read()).hexdigest()
+
+
+class Pyproject(DepHandler):
+    def supported(self) -> bool:
+        return Path("pyproject.toml").exists()
+
+    def requirements(self) -> str:
+        with open("pyproject.toml", "rb") as f:
+            pyproject = tomli.load(f)
+
+        all_deps = pyproject["project"]["dependencies"]
+        return "\n".join(all_deps)
+
+    def reqs_hash(self) -> str:
+        return "reqs-" + str(hash(self.requirements()))
+
+
+def main():
+    # if none of the dependency managers are installed, just run mypy normally
+    for handler in Pipenv(), Pyproject():
+        if handler.supported():
+            if handler.updated():
+                handler.write_reqs()
+                sp.check_call(["pip", "install", "-qr", str(handler.tmp_reqs())], text=True)
+            break
 
     ret = sp.run(["mypy", *sys.argv[1:]], capture_output=False)
     return ret.returncode
